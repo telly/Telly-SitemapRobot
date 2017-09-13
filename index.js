7bddb0199f3d0b160bc486ea62ef305d766afea5
Petr Konupek <petr@konupek.cz>
2017-09-13 22:07:27 +0200

    Remote controll :-)

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+// Crawling process
 var PSG = require("phantomjs-sitemap-generator");
 var p = new PSG( 'https://telly.com', { verbose: true } );
-p.crawl();
+var fs = require('fs');
+
+// Serving sitemap
+const express = require('express')
+const app = express()
+app.use("/sitemap.xml", express.static('sitemap.xml'));
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
+
+// Trigers crawling start
+app.get("/refresh-sitemap", function (req, res) {
+    if (fs.existsSync('sitemap.xml')) {
+        fs.unlink('sitemap.xml');
+    }
+    p.crawl();
+    res.send("Crawling started");
+});
+
+// Retrieve crawling status
+app.get("/crawling-status", function (req, res) {
+    if (fs.existsSync('sitemap.xml')) {
+        res.send('Idle');
+    } else {
+        res.send('Running');
+    }
+})
+
